afefc08a1b96839b8042a34986c906b11046956e
Sam Ricotta <samanthalricotta@gmail.com>
2018-08-16 16:05:03 +0100

    scan and total methods implemented

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -3,10 +3,11 @@ require_relative "../checkout"
 PRODUCTS = [
   {product_code: 001, name: "Very Cheap Chair", price: 9.25},
   {product_code: 002, name: "Little table", price: 45.00},
-  {product_code: 003, name: "Funky light", price: 19.95}
+  {product_code: 003, name: "Funky light", price: 19.95},
 ]
 
 describe "Checkout" do
+
   describe "#scan" do
     it "should add the item to the cart" do
       co = Checkout.new
@@ -18,9 +19,9 @@ describe "Checkout" do
 
   describe "#total" do
     it "should total £0 if the basket is empty" do
-          co = Checkout.new
-          expect(co.total).to eq(0)
-      end
+      co = Checkout.new
+      expect(co.total).to eq(0)
+    end
 
     it "should total £66.78 for the basket: 001, 002, 003" do
       co = Checkout.new
@@ -28,7 +29,7 @@ describe "Checkout" do
       scanned = co.scan(PRODUCTS[1])
       scanned = co.scan(PRODUCTS[2])
       expect(co.total).to eq(66.78)
-  end
+    end
 
     it "should total £36.95 for the basket: 001, 003, 001 " do
       co = Checkout.new
@@ -36,7 +37,7 @@ describe "Checkout" do
       scanned = co.scan(PRODUCTS[2])
       scanned = co.scan(PRODUCTS[0])
       expect(co.total).to eq(36.95)
-  end
+    end
 
     it "should total £73.76 for the basket: 001, 002, 001, 003 " do
       co = Checkout.new

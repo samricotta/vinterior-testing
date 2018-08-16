c12c6960af2c6fd7c11d27395dd66060104b0f28
Sam Ricotta <samanthalricotta@gmail.com>
2018-08-16 12:16:34 +0100

    outline of testing added

---
+++ spec/checkout_spec.rb
@@ -0,0 +1,13 @@
+describe "Checkout" do
+  describe "#scan" do
+    it "should add the item to the cart" do
+      expect().to eq()
+    end
+  end
+
+  describe "#total" do
+    it "should total £66.78 for the basket: 001, 002, 003" do
+      expect().to eq()
+    end
+  end
+end

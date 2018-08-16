329b80edb5b7e7902a2a8e747adcd17f6a205f51
Sam Ricotta <samanthalricotta@gmail.com>
2018-08-16 19:46:12 +0100

    scan and total methods implemented

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,14 +1,14 @@
 require_relative "../checkout"
 
 PRODUCTS = [
-  {product_code: 001, name: "Very Cheap Chair", price: 9.25},
-  {product_code: 002, name: "Little table", price: 45.00},
-  {product_code: 003, name: "Funky light", price: 19.95},
+  {product_code: 001, name: "Very Cheap Chair", price_cents: 925},
+  {product_code: 002, name: "Little table", price_cents: 4500},
+  {product_code: 003, name: "Funky light", price_cents: 1995},
 ]
 
 PROMO = [
-  {type: :percentage, amount: 10, minimum: 60},
-  {type: :price_drop, price: 8.5, product_code: 001, minimum: 2}
+  {type: :price_drop, price_cents: 850, product_code: 001, minimum_items: 2},
+  {type: :percentage, amount_of_discount: 0.1, minimum_cents: 6000}
 ]
 
 describe "Checkout" do
@@ -24,12 +24,12 @@ describe "Checkout" do
 
   describe "#total" do
     it "should total £0 if the basket is empty" do
-      co = Checkout.new(PROMOS)
+      co = Checkout.new(PROMO)
       expect(co.total).to eq(0)
     end
 
     it "should total £66.78 for the basket: 001, 002, 003" do
-      co = Checkout.new
+      co = Checkout.new(PROMO)
       scanned = co.scan(PRODUCTS[0])
       scanned = co.scan(PRODUCTS[1])
       scanned = co.scan(PRODUCTS[2])
@@ -37,7 +37,7 @@ describe "Checkout" do
     end
 
     it "should total £36.95 for the basket: 001, 003, 001 " do
-      co = Checkout.new
+      co = Checkout.new(PROMO)
       scanned = co.scan(PRODUCTS[0])
       scanned = co.scan(PRODUCTS[2])
       scanned = co.scan(PRODUCTS[0])
@@ -45,7 +45,7 @@ describe "Checkout" do
     end
 
     it "should total £73.76 for the basket: 001, 002, 001, 003 " do
-      co = Checkout.new
+      co = Checkout.new(PROMO)
       scanned = co.scan(PRODUCTS[0])
       scanned = co.scan(PRODUCTS[1])
       scanned = co.scan(PRODUCTS[0])
